e0e265cc543f7e8b7fe2e42ad06227dea921a1e8
AMITESH KUSHWAHA <amiteshkushwaha2020@gmail.com>
2025-12-28 12:51:31 +0530

    Update expireProducts.js

--- backend/utils/expireProducts.js
+++ backend/utils/expireProducts.js
@@ -5,15 +5,6 @@ const markExpiredProducts = () => {
     cron.schedule('0 13 * * *'
 , async () => {
         try {
-            const now = new Date();
-            const startOfTodayUTC = new Date(Date.UTC(
-                now.getUTCFullYear(), 
-                now.getUTCMonth(), 
-                now.getUTCDate()
-            ));
-            
-            console.log('Running expired products check at:', new Date().toISOString());
-            console.log('Comparing expiry dates before:', startOfTodayUTC.toISOString());
             
             const result = await Product.updateMany(
                 { },
@@ -25,15 +16,6 @@ const markExpiredProducts = () => {
                     } 
                 }
             );
-            
-            console.log('Expired products updated:', result.modifiedCount);
-            
-            const expiredProducts = await Product.find({
-                expiryDate: { $lt: startOfTodayUTC },
-                isActive: true
-            });
-            
-            console.log('Products that should be expired:', expiredProducts.length);
             
         } catch (err) {
             console.error('Error updating expired products:', err);
