5b3053081619b18698c175d367d0ae0f2c7eb535
AMITESH KUSHWAHA <amiteshkushwaha2020@gmail.com>
2025-12-28 12:57:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 import dotenv from 'dotenv'; 
 import connectDB from './config/db.js';
-import markExpiredProducts from'./utils/expireProducts.js';
+// import markExpiredProducts from'./utils/expireProducts.js';
 
 dotenv.config();
 connectDB(); 
